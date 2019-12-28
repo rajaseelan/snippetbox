ea248caeb4e6d774cb6e7cfff2c9d52f4801e8c8
Rajaseelan Ganeswaran <rajaseelan.ganeswaran@pgs.com>
2019-12-28 12:05:47 +0000

    move routes to application.routes(), main.go only parses runtime config, establish dependencies, run http server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,26 +20,15 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// initialize a new instance of application containing the dependencies
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	// swap the route declarations to use the application struct's methods
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// initialize a new http.Server struct
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(), // call the new app.routes() method
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
