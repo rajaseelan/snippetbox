45762ab0d57b909a3e59bc3d37c6864ca3dcac92
Rajaseelan Ganeswaran <rajaseelan.ganeswaran@pgs.com>
2019-12-28 14:59:02 +0000

    supply model to application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	// new import
 	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql" // new import
+	"github.com/rajaseelan/snippetbox/pkg/models/mysql" // new import
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// Define an application struct to hold application-wide dependencies
+// Add snippet field to application struct
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	// define a new cmd-line flag for MysQL DSN string
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// keep the main() function clean
-	// code for creating connection pool in separate openDB() function
 	db, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
-
-	// close db before main() exits
 	defer db.Close()
 
+	// Initialize a mysql.SnippetModel instance and add it to the application
+	// dependencies
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
