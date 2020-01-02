13ac6f752ea6b37a7e43b00e954b98de2b02bfd8
Rajaseelan Ganeswaran <rajaseelan.ganeswaran@pgs.com>
2020-01-02 23:41:32 +0000

    add query for snippet by ID

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/rajaseelan/snippetbox/pkg/models"
 )
 
 // change the signature of the home handler fo it is defined as a method
@@ -40,7 +43,20 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID: %d...", id)
+	// Use SNnippetModel's Get() method to retrieve data for a specific
+	// record based on it's ID
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body
+	fmt.Fprintf(w, "%v", s)
 }
 
 // change signature of createSnippet to be defined against
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,7 +2,7 @@ package mysql
 
 import (
 	"database/sql"
-
+	"errors"
 	"github.com/rajaseelan/snippetbox/pkg/models"
 )
 
@@ -36,7 +36,32 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // Get return a specific snippet based on its id
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+
+	// SQL Statement we want to execute
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// use QueryRow() on the connection pool
+	// returns a pointer to sql.Row object
+	row := m.DB.QueryRow(stmt, id)
+
+	// Initialize snippet to a new zeroed Snippet Struct
+	s := &models.Snippet{}
+
+	// use row.Scan() to copy values from each field to the corresponding snippet struct field
+	// args to row.Scan are pointers, no of clumns must be the same
+	// as no of columns returned
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	// everything went ok, return the snippet object
+	return s, nil
 }
 
 // Latest return 10 most recently created snippets
